7089821bd9637cc4a2749a866c313a7c034e510b
devhattrick <peerapol.panmuang@gmail.com>
2022-03-09 22:48:33 +0700

    learn 1

--- my-test/src/app/app.component.ts
+++ my-test/src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  value=0;
   title = 'my-test';
+  myName ="Peerapol";
+  peerapolConsole(){
+    console.log('Hello Hattrick')
+  }
+  changeName(name:string){
+    this.myName=name
+  }
+  calSum(value:string){
+    if(isNaN(this.value=(parseInt(value) *3)/4)){
+      alert('Invalid')
+    }else{ this.value=(parseInt(value) *3)/4}
+   
+  }
 }
